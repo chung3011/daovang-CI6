68ddb17726bc0cb299580711a156524c7a0cfa94
danle <danlinhle13@gmail.com>
2018-07-02 22:04:07 +0700

    fix bugs with bombs

--- src/Game/Bomb/BombGenerator.java
+++ src/Game/Bomb/BombGenerator.java
@@ -1,10 +1,14 @@
 package Game.Bomb;
 
 import Action.ActionAdapter;
+import Action.LimitAction;
 import Action.SequenceAction;
 import Action.WaitAction;
 import Base.GameObject;
 import Base.GameObjectManager;
+import Constant.Constant;
+import Game.Level;
+import Game.ObjectsToCatch.LargeObject.LargeObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,34 +23,37 @@ public class BombGenerator extends GameObject {
     }
 
     public void createAction() {
-        this.addAction(
-                new SequenceAction(
-                        new WaitAction(5),
-                        new ActionAdapter() {
-
-                            private List<Bomb> list = new ArrayList<>();
-                            private int count = 0;
-
-                            @Override
-                            public boolean run(GameObject owner) {
-                                if (list.isEmpty()) {
-                                    Bomb bom = GameObjectManager.instance.recycle(Bomb.class);
-                                    bom.position.set(30 + random.nextInt(950),100 +  random.nextInt(200));
-                                    this.count += 1;
-                                    list.add(bom);
-                                }
-
-                                list.removeIf(mediumObject -> !mediumObject.isAlive);
-
-                                return this.count == 15;
-                            }
+        if (Level.level == 1) {
+            this.addAction(
+                    new ActionAdapter() {
+                        @Override
+                        public boolean run(GameObject owner) {
+                            Bomb bomb = GameObjectManager.instance.recycle(Bomb.class);
+                            bomb.position.set(60 + random.nextInt(900),160 +  random.nextInt(380));
+                            bomb.boxCollider.position.set((int) bomb.position.x - 20, (int) bomb.position.y - 20);
+                            return true;
                         }
-                )
-        );
+                    });
+        }
+
+        if (Level.level == 2) {
+            this.addAction(new LimitAction(
+                    new ActionAdapter() {
+                        @Override
+                        public boolean run(GameObject owner) {
+                            LargeObject largeObject = GameObjectManager.instance.recycle(LargeObject.class);
+                            largeObject.position.set(60 + random.nextInt(900),160 +  random.nextInt(380));
+                            largeObject.boxCollider.position.set((int) largeObject.position.x - 20, (int) largeObject.position.y - 20);
+                            return true;
+                        }
+                    },
+                    2)
+            );
+        }
     }
 
-    @Override
-    public void run() {
-        super.run();
+        @Override
+        public void run () {
+            super.run();
+        }
     }
-}
